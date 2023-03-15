95dc1209ad7c999f63ebb68b209e44fbe412adba
Jordan Pears <jordanpears@fb.com>
2023-03-15 18:26:19 +0000

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert, Button, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title={"Press Me!"} onPress={() => Alert.alert('Simple Button pressed')}></Button>
       <StatusBar style="auto" />
     </View>
   );
